849ce12e9a6bfce86eff7a2984dad5c2b41cd27d
VID\lass4479 <lwjmarketing@outlook.com>
2018-01-10 10:55:18 +0100

    Added KundeOrdreTabel + OrdreDetaljerTabel

---
+++ LyngbyBrygRepo/Models/OrdreDetaljerTabel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Duser;
+
+namespace LyngbyBrygRepo
+{
+    public class OrdreDetaljerTabel
+    {
+        public int ID { get; set; }
+        public int ProduktID { get; set; }
+        public int Antal { get; set; }
+        public int BrugerID { get; set; }
+        public int KategoriID { get; set; }
+        public DateTime Dato { get; set; }
+        public int OrdreID { get; set; }
+    }
+}
