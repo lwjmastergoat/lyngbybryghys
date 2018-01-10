4d9160efb189653ff34e6e0700bb4061becd439e
chri24m5 <chri24m5@VIDHQFJS32>
2018-01-10 10:22:03 +0100

    ddmf tilføjet

---
+++ LyngbyBrygRepo/DDMF/Conn.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SqlClient;
+
+namespace Duser
+{
+    public static class Conn
+    {
+        // Koden kan bruges fridt så længe denne tekst bliver i toppen af filen.
+        // Copyright 2016 E-train I/S, Udviklet af Henrik Obsen
+
+        /// <summary>
+        /// Metoden indeholder formindelsesindstillinger til databaen
+        /// </summary>
+        /// <returns>Retunere en MS SQL connection string</returns>
+        public static SqlConnection GetCon()
+        {
+            SqlConnection con = new SqlConnection("server=IP_ADRESSE;database=DATABASE;uid=BRUGER;pwd=KODEORD;MultipleActiveResultSets=True");
+            return con;
+        }
+
+        /// <summary>
+        /// Metoden retunere en åben forbindelse til databasen der er defineret i GetCon()
+        /// </summary>
+        /// <returns>Retunere en åben forbindelse til databasen</returns>
+        public static SqlConnection CreateConnection()
+        {
+            var cn = GetCon();
+            cn.Open();
+            return cn;
+        }
+    }
+}
+
+
