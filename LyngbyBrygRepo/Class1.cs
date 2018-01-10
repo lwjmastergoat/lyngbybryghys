414048db1f0c9a9cad5894702959733adfd5fbc4
chri24m5 <chri24m5@VIDHQFJS32>
2018-01-10 09:23:07 +0100

    ændringer

---
+++ LyngbyBrygRepo/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LyngbyBrygRepo
+{
+    public class Class1
+    {
+
+     HRallo ......
+         
+    }
+}
