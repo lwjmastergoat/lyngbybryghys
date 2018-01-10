ee19535d5deb858a02e1af2fac5942587c178751
anet2329 <anet2329@VIDGWTDGC2>
2018-01-10 11:22:25 +0100

    ProduktTabel

---
+++ LyngbyBrygRepo/Models/ProduktTabel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Duser;
+
+namespace LyngbyBrygRepo
+{
+    public class ProduktTabel
+    {
+        public int ID { get; set; }
+        public int KategoriID { get; set; }
+        public string Navn { get; set; }
+        public string Beskrivelse { get; set; }
+        public string Billede { get; set; }
+        public decimal Pris { get; set; }
+
+    }
+}
