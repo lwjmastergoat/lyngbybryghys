c297e3a715293b3deafa06862589e6424dd9f47d
chri24m5 <chri24m5@VIDHQFJS32>
2018-01-12 10:17:17 +0100

    produktkategorijoin

---
+++ LyngbyBrygRepo/Models/R_Models/ProduktKategoriJoin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Duser;
+using System.Web;
+using LyngbyBrygRepo.Models;
+
+namespace LyngbyBrygRepo.Models
+{
+    public class ProduktKategoriJoin
+    {
+        public int ID { get; set; }
+
+        public int KategoriID { get; set; }
+
+        public string Navn { get; set; }
+
+        public string Beskrivelse { get; set; }
+
+        public string Billede { get; set; }
+
+        public decimal Pris { get; set; }
+
+        // Og fra kategori tabellen:
+
+        public int ID { get; set; }
+
+        public string Navn { get; set; }
+
+
+
+
+    }
+}
